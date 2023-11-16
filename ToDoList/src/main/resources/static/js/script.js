58c63b524a6bb272fb5a97cbfd5cf51ef8110313
Martyushev Leoned <56407820+martyushevleoned@users.noreply.github.com>
2023-11-16 22:09:07 +0500

    promise

--- ToDoList/src/main/resources/static/js/script.js
+++ ToDoList/src/main/resources/static/js/script.js
@@ -10,41 +10,38 @@ async function newProject() {
     });
 
     fetch(url + '?' + params).then(
-        r => addProject(project, r.text()),
+        r => r.text().then(id => addProject(project, id)),
         r => alert('Ошибка HTTP: ' + r.status)
     );
 }
 
-function addProject(projectName, promise) {
+function addProject(projectName, id) {
 
-    promise.then(id => {
+   console.log(id)
 
-        console.log(id)
+   let pn = document.createElement('div');
+   pn.className = 'content';
+   pn_sp = document.createElement('span');
+   pn_sp.innerHTML += projectName;
+   pn.appendChild(pn_sp);
 
-        let pn = document.createElement('div');
-        pn.className = 'content';
-        pn_sp = document.createElement('span');
-        pn_sp.innerHTML += projectName;
-        pn.appendChild(pn_sp);
+   let ct = document.createElement('div');
+   ct.className = 'content';
+   ct_sp = document.createElement('span');
+   ct_sp.innerHTML += '0 tasks';
+   ct.appendChild(ct_sp);
 
-        let ct = document.createElement('div');
-        ct.className = 'content';
-        ct_sp = document.createElement('span');
-        ct_sp.innerHTML += '0 tasks';
-        ct.appendChild(ct_sp);
+   let dv = document.createElement('div');
+   dv.className = 'content no-padding';
+   dv_bt = document.createElement('button');
+   dv_bt.innerHTML += 'open';
+   dv.appendChild(dv_bt);
 
-        let dv = document.createElement('div');
-        dv.className = 'content no-padding';
-        dv_bt = document.createElement('button');
-        dv_bt.innerHTML += 'open';
-        dv.appendChild(dv_bt);
+   let bc = document.createElement('div');
+   bc.className = 'body-content';
+   bc.appendChild(pn);
+   bc.appendChild(ct);
+   bc.appendChild(dv);
 
-        let bc = document.createElement('div');
-        bc.className = 'body-content';
-        bc.appendChild(pn);
-        bc.appendChild(ct);
-        bc.appendChild(dv);
-
-        document.getElementById('body').appendChild(bc);
-    });
+   document.getElementById('body').appendChild(bc);
 }
