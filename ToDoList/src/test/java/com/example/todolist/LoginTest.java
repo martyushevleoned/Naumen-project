95e360583b9bcfbe2c6ce3e8273a89453cc1f181
Martyushev Leoned <56407820+martyushevleoned@users.noreply.github.com>
2023-12-09 00:17:26 +0500

    tests

---
+++ ToDoList/src/test/java/com/example/todolist/LoginTest.java
@@ -0,0 +1,36 @@
+package com.example.todolist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.todolist.controller.RegistrationController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private RegistrationController registrationController;
+
+    @Test
+    public void test() throws Exception {
+        assertThat(registrationController).isNotNull();
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Projects")));
+    }
+}
