807159ba0102cbfc3e608a3ec2306e612a1ab9e3
Martyushev Leoned <56407820+martyushevleoned@users.noreply.github.com>
2023-12-17 23:54:15 +0500

    tests

--- ToDoList/src/test/java/com/example/todolist/LoginTest.java
+++ ToDoList/src/test/java/com/example/todolist/LoginTest.java
@@ -13,12 +13,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(value = {"/create-user-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/create-user-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class LoginTest {
 
     @Autowired
